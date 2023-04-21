8a79afa8ef5d9c9660c7be3be101cf0104bd33c7
Lauren Giordano <laurengiordano94@gmail.com>
2023-04-21 12:20:47 +1000

    built out RecipeList/Item layout

--- src/Components/RecipeItem.js
+++ src/Components/RecipeItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-
+import recipePlaceholder from "../recipePlaceholder.jpeg";
 
 function RecipeItem ({name, image, url, madeCount, id}) {
 
@@ -13,14 +13,14 @@ function RecipeItem ({name, image, url, madeCount, id}) {
     return (
         <Col >
             <Card className="h-100" >
-                <Card.Img variant="top" src={image} alt={name} style={imageStyles} />
+                <Card.Img variant="top" src={image || recipePlaceholder} alt={name} style={imageStyles} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
                     <Card.Text>
-                        <a href={url} target="_blank" >Take me to the recipe!</a>
-                        {(madeCount > 0) ? <p>`Made ${madeCount} times!`</p> : <p>"You've not made this yet"</p>}
+                        <a href={url} target="_blank" >Take me to the recipe!</a><br/>
+                        {(madeCount > 0) ? <span>Made {madeCount} times!</span> : <span>You've not made this yet</span>}
                     </Card.Text>
-                    <Button variant="primary">Increase Made Count</Button>
+                    <Button variant="primary">Increment Count</Button>
                 </Card.Body>
             </Card>
         </Col>
